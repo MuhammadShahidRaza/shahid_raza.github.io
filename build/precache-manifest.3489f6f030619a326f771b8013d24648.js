03df7e967627036051f634589bbe2d234dbcdb13
Muhammad Shahid Raza <Muhammadshahidraza8@gmail.com>
2023-11-05 04:52:02 +0500

    deploy

---
+++ build/precache-manifest.3489f6f030619a326f771b8013d24648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "237f469a3d9c34b305ed",
+    "url": "/static/css/main.52eba9e7.chunk.css"
+  },
+  {
+    "revision": "237f469a3d9c34b305ed",
+    "url": "/static/js/main.237f469a.chunk.js"
+  },
+  {
+    "revision": "8765ab557c059da2471a",
+    "url": "/static/js/1.8765ab55.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab77620327dce04efed6fcc536a06ed0",
+    "url": "/index.html"
+  }
+];
